b745b1f284429469dc07a296bcfca4834f27c045
Ursa Pertot <ursa.pertot@gmail.com>
2018-05-09 00:28:21 +0200

    Graf racionalne funkcije in risanje grafov v latexu

--- generiranje.py
+++ generiranje.py
@@ -13,23 +13,25 @@ def preveri(pogoj):
 
 ############################## Besedila rešitev
 slovarKratkihNalog = {"poisci_nicle_polinoma": "Poišči vse ničle polinoma $p(x)= %s$.",
-                      "racionalna" : "Izračunaj ničle in pole racionalne funkcije $q(x) = %s$."} #TODO a je smiselno imeti te slovarje
+                      "racionalna": "Izračunaj ničle in pole racionalne funkcije $q(x) = %s$."}  # TODO a je smiselno imeti te slovarje
 
-slovarDolgihNalog= {"poisci_nicle_polinoma": "Poišči ničle polinomov:\n\\begin{enumerate}\n"}
+slovarDolgihNalog = {"poisci_nicle_polinoma": "Poišči ničle polinomov:\n\\begin{enumerate}\n"}
+
 
 ############################## Naloge
 
-def racionalna(seme = "Padawan",razpon=(-9, 9), st_nicel=3, st_polov=3):
+def racionalna(seme="Padawan", razpon=(-9, 9), st_nicel=3, st_polov=3):
     random.seed(seme)
     od, do = razpon
     nicle = [random.randint(od, do) for _ in range(st_nicel)]
     poli = [random.randint(od, do) for _ in range(st_polov)]
-    #preveri(set(nicle) & set(poli) == set())
+    # preveri(set(nicle) & set(poli) == set()) #TODO ponavlej dokler ni vredu
     x = sympy.symbols('x')
     stevec = '*'.join('(x-{0})'.format(nicla) for nicla in nicle)
     imenovalec = '*'.join('(x-{0})'.format(pol) for pol in poli)
-    naloga = sympy.latex(sympy.expand(stevec)/sympy.expand(imenovalec))
-    resitev = "Ničle funkcije so " + ', '.join('{0}'.format(nicla) for nicla in nicle)+", poli pa so "+ ', '.join('{0}'.format(pol) for pol in poli) + "."
+    naloga = sympy.expand(stevec) / sympy.expand(imenovalec)
+    resitev = "Ničle funkcije so " + ', '.join('{0}'.format(nicla) for nicla in nicle) + ", poli pa so " + ', '.join(
+        '{0}'.format(pol) for pol in poli) + "."
     print(naloga)
     return (naloga, resitev)
     # return {
@@ -38,47 +40,70 @@ def racionalna(seme = "Padawan",razpon=(-9, 9), st_nicel=3, st_polov=3):
     #     'poli': poli,
     # }
 
-def nalogaRacionalna(seme = "Padawan",razpon=(-9, 9), st_nicel=3, st_polov=3, primeri = 1):
-    if primeri ==1:
+
+def nalogaNiclePoliRacionalne(seme="Padawan", razpon=(-9, 9), st_nicel=3, st_polov=3, primeri=1):
+    if primeri == 1:
         naloga, resitev = racionalna(seme, razpon, st_nicel, st_polov)
-        besedilo_naloge = "Izračunaj ničle in pole funkcije $q(x) = %s$."% naloga
+        besedilo_naloge = "Izračunaj ničle in pole funkcije $q(x) = %s$." % sympy.latex(naloga)
         besedilo_resitve = resitev
     else:
-        besedilo_naloge= "Poišči ničle in pole funkcij:\n \\begin{enumerate}\n"
-        besedilo_resitve ="Rešitve: \n\\begin{enumerate}\n"
-        for i in range(1, primeri+1):
-            seme +=str(i)
+        besedilo_naloge = "Poišči ničle in pole funkcij:\n \\begin{enumerate}\n"
+        besedilo_resitve = "Rešitve: \n\\begin{enumerate}\n"
+        for i in range(1, primeri + 1):
+            seme += str(i)
             naloga, resitev = racionalna(seme, razpon, st_nicel, st_polov)
-            besedilo_naloge +="\\item $q(x) = %s$\n" % naloga
-            besedilo_resitve += "\\item $%s$\n" %resitev
+            besedilo_naloge += "\\item $q(x) = %s$\n" % sympy.latex(naloga)
+            besedilo_resitve += "\\item $%s$\n" % resitev
         besedilo_resitve += "\end{enumerate}"
         besedilo_naloge += "\end{enumerate}"
-    return (besedilo_naloge, besedilo_resitve )
+    return (besedilo_naloge, besedilo_resitve)
 
-def poisci_nicle_polinoma(od=-7, do=7 , stevilo_nicel=3, seme = "Padawan", primeri = 1):
+
+def nalogaGrafRacionalne(seme="Padawan", razpon=(-9, 9), st_nicel=3, st_polov=3, primeri=1):
+    if primeri == 1:
+        naloga, resitev = racionalna(seme, razpon, st_nicel, st_polov)
+        besedilo_naloge = "Nariši graf funkcije $q(x) = %s$." % sympy.latex(naloga)
+        graf_funkcije = str(naloga).replace("**", "^")
+        besedilo_resitve = "\\begin{tikzpicture}\n\\begin{axis}[axis lines=middle, xlabel=$x$, ylabel=$y$]\n\\addplot[domain =-7:7, color=black]{%s};\n\\end{axis}\n\\end{tikzpicture}\n" % graf_funkcije
+    else:
+        besedilo_naloge = "Nariši grafe funkcij:\n \\begin{enumerate}\n"
+        besedilo_resitve = "Rešitve: \n\\begin{enumerate}\n"
+        for i in range(1, primeri + 1):
+            seme += str(i)
+            naloga, resitev = racionalna(seme, razpon, st_nicel, st_polov)
+            besedilo_naloge += "\item $q(x) = %s$\n" % sympy.latex(naloga)
+            graf_funkcije = str(naloga).replace("**", "^")
+            besedilo_resitve += "\item \\begin{tikzpicture}\n\\begin{axis}[axis lines=middle, xlabel=$x$, " \
+                                "ylabel=$y$]\n\\addplot[domain =-7:7, color=black]{%s};\n\end{axis}\n\end{tikzpicture}\n" % graf_funkcije  # TODO nariši asimpotote
+        besedilo_resitve += "\end{enumerate}"
+        besedilo_naloge += "\end{enumerate}"
+    return (besedilo_naloge, besedilo_resitve)
+
+
+def poisci_nicle_polinoma(od=-7, do=7, stevilo_nicel=3, seme="Padawan", primeri=1):
     random.seed(seme)
-    #print(seme)
+    # print(seme)
     if primeri == 1:
         nicle = [random.randint(od, do) for _ in range(stevilo_nicel)]  # Naredi seznam treh celih ničel
         x = sympy.symbols('x')
         polinom = '*'.join('(x-{0})'.format(nicla) for nicla in nicle)
         naloga = slovarKratkihNalog["poisci_nicle_polinoma"] % sympy.latex(sympy.expand(polinom))
-        #print(naloga)
+        # print(naloga)
         resitev = "Ničle polinoma so " + ', '.join('{0}'.format(nicla) for nicla in nicle) + "."
         return naloga, resitev
     else:
-        naloge= slovarDolgihNalog["poisci_nicle_polinoma"]
-        resitve="\\begin{enumerate}"
+        naloge = slovarDolgihNalog["poisci_nicle_polinoma"]
+        resitve = "\\begin{enumerate}"
         for i in range(primeri):
-            random.seed(seme+str(i))
+            random.seed(seme + str(i))
             nicle = [random.randint(od, do) for _ in
                      range(stevilo_nicel)]  # Naredi seznam treh celih ničel
             x = sympy.symbols('x')
             polinom = '*'.join('(x-{0})'.format(nicla) for nicla in nicle)
             naloga = sympy.latex(sympy.expand(polinom))
-            naloge += "\item $p(x)=" +naloga +"$\n"
-            #print(naloga)
-            resitve += "Ničle polinoma so " + ', '.join('{0}'.format(nicla) for nicla in nicle) + ".\n"
+            naloge += "\item $p(x)=" + naloga + "$\n"
+            # print(naloga)
+            resitve += "\item Ničle polinoma so " + ', '.join('{0}'.format(nicla) for nicla in nicle) + ".\n"
         naloge += "\end{enumerate}"
         resitve += "\end{enumerate}"
         return naloge, resitve
@@ -98,11 +123,10 @@ def generiraj(naloga, vzorec, **parametri):
     return vzorec.format(**podatki)
 
 
-#print(generiraj(racionalna, 'Funkcija {funkcija} ima ničle: {nicle}', st_polov=5))
+# print(generiraj(racionalna, 'Funkcija {funkcija} ima ničle: {nicle}', st_polov=5))
 
-#print(poisci_nicle_polinoma( "B",-20,10,4))
-#print(poisci_nicle_polinoma(primeri=2))
+# print(poisci_nicle_polinoma( "B",-20,10,4))
+# print(poisci_nicle_polinoma(primeri=2))
 
-print(nalogaRacionalna(primeri=3))
-
-
+# print(nalogaRacionalna(primeri=3))
+print(racionalna())
--- izpisi_naloge.py
+++ izpisi_naloge.py
@@ -4,14 +4,17 @@ import generiranje
 import os
 import shutil
 
-def sestaviVseTeste(datoteka_seznam_dijakov, naloge, ime_naloge=date.today().strftime("%d-%B-%Y"), zdruzene_resitve=True): ###TODO če ni seznama naredi 1 test
-    stevilo_nalog = len(naloge)
-    seznam_ljudi = open(datoteka_seznam_dijakov, encoding="utf8").readlines() #TODO close? #TODO če ni seznama naredi samo 1 test
-    podmapa = ime_naloge
-    potResitve = podmapa+"/rešitve"
-    potNaloge = podmapa+"/naloge"
 
-    if os.path.exists(podmapa):  #Zbriše staro mapo s tem imenom in ustvari novo
+def sestaviVseTeste(datoteka_seznam_dijakov, naloge, ime_naloge=date.today().strftime("%d-%B-%Y"),
+                    zdruzene_resitve=True):  ###TODO če ni seznama naredi 1 test
+    stevilo_nalog = len(naloge)
+    seznam_ljudi = open(datoteka_seznam_dijakov,
+                        encoding="utf8").readlines()  # TODO close? #TODO če ni seznama naredi samo 1 test
+    podmapa = ime_naloge
+    potResitve = podmapa + "/rešitve"
+    potNaloge = podmapa + "/naloge"
+
+    if os.path.exists(podmapa):  # Zbriše staro mapo s tem imenom in ustvari novo
         shutil.rmtree(podmapa)
         os.makedirs(podmapa)
         os.makedirs(potNaloge)
@@ -22,81 +25,84 @@ def sestaviVseTeste(datoteka_seznam_dijakov, naloge, ime_naloge=date.today().str
         os.makedirs(potResitve)
 
     napisiVzorecTesta(stevilo_nalog, podmapa)
-    napisiVzorecResitev(stevilo_nalog,podmapa)
+    napisiVzorecResitev(stevilo_nalog, podmapa)
     for dijak in seznam_ljudi:
         dijak = dijak.strip()
-        napisiTest(podmapa+"/vzorec_testa.txt",podmapa+"/vzorec_resitev.txt", naloge, dijak, potNaloge, potResitve)
+        napisiTest(podmapa + "/vzorec_testa.txt", podmapa + "/vzorec_resitev.txt", naloge, dijak, potNaloge, potResitve)
     if zdruzene_resitve:
-        print("TODO") #TODO združevanje rešitev
-        #zdruziVseResitve()
+        print("TODO")  # TODO združevanje rešitev
+        # zdruziVseResitve()
     else:
         vzorec_resitev = napisiVzorecResitev(stevilo_nalog, podmapa)
-        #todo vpiši rešitve iz textov
-        #todo delete text files
+        # todo vpiši rešitve iz textov
+        # todo delete text files
 
     return "bu"
 
-def napisiTest(vzorec_testa, vzorec_resitev, naloge, dijak, potNaloge, potResitve): #Napiše naloge in njihove rešitve #TODO rešitve kot txt?
+
+def napisiTest(vzorec_testa, vzorec_resitev, naloge, dijak, potNaloge,
+               potResitve):  # Napiše naloge in njihove rešitve #TODO rešitve kot txt?
     ### Napiše Naloge (že v tex obliki)
     dokumentNaloge = open(vzorec_testa, encoding="utf8")
     vzorecNaloge = Template(dokumentNaloge.read())
     dokumentResitve = open(vzorec_resitev, encoding="utf8")
     vzorecResitve = Template(dokumentResitve.read())
 
-    slovar_za_vstavljanje_nalog={"ime_dijaka": dijak}
+    slovar_za_vstavljanje_nalog = {"ime_dijaka": dijak}
     slovar_za_vstavljanje_resitev = {"ime_dijaka": dijak}
     for zaporedna_stevilka, naloga in enumerate(naloge):
-        (besediloNaloge, besediloResitve) = naloga(seme=dijak, primeri=1) #TODO Kako podaš že nekej argumentov
-        slovar_za_vstavljanje_nalog['naloga%d'%(zaporedna_stevilka +1)] = besediloNaloge
-        slovar_za_vstavljanje_resitev['naloga%d' % (zaporedna_stevilka + 1)] = besediloNaloge
+        (besediloNaloge, besediloResitve) = naloga(seme=dijak, primeri=2)  # TODO Kako podaš že nekej argumentov
+        slovar_za_vstavljanje_nalog['naloga%d' % (zaporedna_stevilka + 1)] = besediloNaloge
+        slovar_za_vstavljanje_resitev['naloga%d' % (
+                    zaporedna_stevilka + 1)] = besediloNaloge  # TODO izmenično primer/rešitev če imaš namen izpisovat naloge v rešitvah
         slovar_za_vstavljanje_resitev['resitev%d' % (zaporedna_stevilka + 1)] = besediloResitve
 
-    #naloga1 = generiranje.poisci_nicle_polinoma()[0]
-    #naloga2 = generiranje.racionalna()["funkcija"]
-    #slovar_za_vstavljanje_nalog = {"naslov":naslov, "naloga1":naloga1, "naloga2":naloga2}
+    # naloga1 = generiranje.poisci_nicle_polinoma()[0]
+    # naloga2 = generiranje.racionalna()["funkcija"]
+    # slovar_za_vstavljanje_nalog = {"naslov":naslov, "naloga1":naloga1, "naloga2":naloga2}
     koncni_dokumentNaloge = vzorecNaloge.substitute(slovar_za_vstavljanje_nalog)
-    koncniDokumentResitve =vzorecResitve.substitute(slovar_za_vstavljanje_resitev)
-    #print(koncni_dokumentNaloge)
+    koncniDokumentResitve = vzorecResitve.substitute(slovar_za_vstavljanje_resitev)
+    # print(koncni_dokumentNaloge)
     dokumentNaloge.close()
     dokumentResitve.close()
 
-    izhodneNaloge = open("%s/%s.tex" %(potNaloge, dijak), "w+", encoding="utf8")
+    izhodneNaloge = open("%s/%s.tex" % (potNaloge, dijak), "w+", encoding="utf8")
     izhodneNaloge.write(koncni_dokumentNaloge)
     izhodneNaloge.close()
 
-    izhodneResitve = open("%s/%s.tex" %(potResitve, dijak), "w+", encoding="utf8")
+    izhodneResitve = open("%s/%s.tex" % (potResitve, dijak), "w+", encoding="utf8")
     izhodneResitve.write(koncniDokumentResitve)
     izhodneResitve.close()
 
 
-
-
 def zdruziVseResitve(vseResitve):
-    return #TODO
-
-def napisiVzorecTesta(steviloNalog,podmapa):
-    with open("%s/vzorec_testa.txt" %podmapa, "w+",encoding="utf8") as vzorec_testa:
-        vzorec_testa.writelines(["\\documentclass{article}\n\n","\\usepackage[utf8]{inputenc}\n\n","\\begin{document}\n\n",
-                                "\\title{%s}\n"%podmapa,"\\author{$ime_dijaka}\n","\\maketitle\n", "\\begin{enumerate}\n"])
-        for i in range(1,steviloNalog+1):
-            vzorec_testa.write("\\item $naloga%d\n\n" %i)
-        vzorec_testa.writelines(["\\end{enumerate}\n","\\end{document}"])
-
-def napisiVzorecResitev(steviloNalog, podmapa):
-    with open("%s/vzorec_resitev.txt" %podmapa, "w+",encoding="utf8") as vzorec_resitev:
-        vzorec_resitev.writelines(["\\documentclass{article}\n\n","\\usepackage[utf8]{inputenc}\n\n","\\begin{document}\n\n",
-                                "\\title{Rešitve: %s}\n"%podmapa,"\\author{$ime_dijaka}\n","\\maketitle\n", "\\begin{enumerate}\n"])
-        for i in range(1,steviloNalog+1):
-            vzorec_resitev.write("\\item $naloga%d\n" %i)
-            vzorec_resitev.write("$resitev%d\n\n" %i)
-        vzorec_resitev.writelines(["\\end{enumerate}\n","\\end{document}"])
-
-sestaviVseTeste("dijaki.txt",[generiranje.poisci_nicle_polinoma,generiranje.nalogaRacionalna],"Test za testiranje")
-#TODO Kako lahko daš nekatere zahteve notr
-#TODO Če daš dvakrat isto nalogo na seznam vzame enako seme - torej enaki primeri
+    return  # TODO
 
 
+def napisiVzorecTesta(steviloNalog, podmapa):  # TODO grafi
+    with open("%s/vzorec_testa.txt" % podmapa, "w+", encoding="utf8") as vzorec_testa:
+        vzorec_testa.writelines(
+            ["\\documentclass{article}\n\n", "\\usepackage[utf8]{inputenc}\n\n", "\\usepackage{tikz}\n",
+             "\\usepackage{pgfplots}\n\n", "\\pgfplotsset{compat=1.16}\n\n", "\\begin{document}\n\n",
+             "\\title{%s}\n" % podmapa, "\\author{$ime_dijaka}\n", "\\maketitle\n", "\\begin{enumerate}\n"])
+        for i in range(1, steviloNalog + 1):
+            vzorec_testa.write("\\item $naloga%d\n\n" % i)
+        vzorec_testa.writelines(["\\end{enumerate}\n", "\\end{document}"])
 
 
+def napisiVzorecResitev(steviloNalog, podmapa):  # TODO grafi
+    with open("%s/vzorec_resitev.txt" % podmapa, "w+", encoding="utf8") as vzorec_resitev:
+        vzorec_resitev.writelines(
+            ["\\documentclass{article}\n\n", "\\usepackage[utf8]{inputenc}\n\n", "\\usepackage{tikz}\n",
+             "\\usepackage{pgfplots}\n\n", "\\pgfplotsset{compat=1.16}\n\n", "\\begin{document}\n\n",
+             "\\title{Rešitve: %s}\n" % podmapa, "\\author{$ime_dijaka}\n", "\\maketitle\n", "\\begin{enumerate}\n"])
+        for i in range(1, steviloNalog + 1):
+            vzorec_resitev.write("\\item $naloga%d\n" % i)
+            vzorec_resitev.write("$resitev%d\n\n" % i)
+        vzorec_resitev.writelines(["\\end{enumerate}\n", "\\end{document}"])
 
 
+sestaviVseTeste("dijaki.txt", [generiranje.poisci_nicle_polinoma, generiranje.nalogaGrafRacionalne],
+                "Test za testiranje")
+# TODO Kako lahko daš nekatere zahteve notr
+# TODO Če daš dvakrat isto nalogo na seznam vzame enako seme - torej enaki primeri
