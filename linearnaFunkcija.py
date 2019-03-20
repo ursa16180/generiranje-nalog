a8cd16627b2cc172440dad77c5df6e1a00d1e0fb
Ursa Pertot <ursa.pertot@gmail.com>
2019-03-20 20:30:12 +0100

    Preurejanje sklopv

--- linearnaFunkcija.py
+++ linearnaFunkcija.py
@@ -33,7 +33,72 @@ def implicinaPremica():
     return [a, b, c, implicitna]
 
 
+def skoziTocki(x1, y1, x2, y2):
+    x = sympy.symbols('x')
+    k = (y2 - y1) / (x2 - x1)
+    n = y1 - k * x1
+    return [k, n, k * x + n]
+
+
 # ~~~~~Posamezne naloge iz poglavja Linearna funkcija
+
+class VzorecNaloge(Naloga):
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self.besedilo_posamezne = jinja2.Template(
+            r'''Zapiši enačbo premice skozi točki $A({{latex(naloga.x1)}},{{latex(naloga.y1)}})$ in $B({{latex(naloga.x2)}},{{latex(naloga.y2)}})$.''')
+        self.besedilo_vecih = jinja2.Template(r'''Zapiši enačbo premice skozi točki:
+        \begin{enumerate}
+        {% for naloga in naloge %}
+        \item $A({{latex(naloga.x1)}},{{latex(naloga.y1)}})$, $B({{latex(naloga.x2)}},{{latex(naloga.y2)}})$
+        {% endfor %}
+        \end{enumerate}
+        ''')
+        self.resitev_posamezne = jinja2.Template(r'''$y={{naloga.premica}}$''')
+        self.resitev_vecih = jinja2.Template(r'''
+        \begin{enumerate}
+         {% for naloga in naloge %}
+         \item $y={{naloga.premica}}$
+         {% endfor %}
+         \end{enumerate}
+         ''')
+
+    def poskusi_sestaviti(self):
+        x1 = random.choice(seznamPolovick(-5, 5) + seznamTretinj(-5, 5))
+        y1 = random.choice(seznamPolovick(-5, 5) + seznamTretinj(-5, 5))
+        x2 = random.randint(-10, 10)
+        y2 = random.randint(-10, 10)
+        preveri(x1 != x2 and y1 != y2)  # Preveri, da sta 2 vzporedni točki in nista vzporedni osem
+        premica = sympy.latex(skoziTocki(x1, y1, x2, y2)[-1])
+        return {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'premica': premica}
+
+class RazdaljaMedTockama(Naloga):  # Todo težja racionalne koordinate? #TODO preveri jinja latex
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self.besedilo_posamezne = jinja2.Template(
+            r'''Natančno izračunaj razdaljo med točkama $A({{naloga.x1}},{{naloga.y1}})$ in $B({{naloga.x2}},{{naloga.y2}})$.''')
+        self.besedilo_vecih = jinja2.Template(r'''Natančno izračunaj razdaljo med točkama
+                                              \begin{enumerate}
+                                              {% for naloga in naloge %}
+                                              \item $A({{naloga.x1}},{{naloga.y1}})$ in $B({{naloga.x2}},{{naloga.y2}})$
+                                              {% endfor %}
+                                              \end{enumerate}'''
+                                              )
+        self.resitev_posamezne = jinja2.Template(r'''$d(A,B)={{naloga.razdalja}}$''')
+        self.resitev_vecih = jinja2.Template(r'''\begin{enumerate}
+                                             {% for naloga in naloge %}
+                                             \item $d(A,B)={{naloga.razdalja}}$
+                                             {% endfor %}
+                                             \end{enumerate}''')
+
+    def poskusi_sestaviti(self):
+        x1 = izberiKoordinato()
+        y1 = izberiKoordinato()
+        x2 = izberiKoordinato()
+        y2 = izberiKoordinato()
+        preveri(x1 != x2 and y1 != y2)
+        razdalja = sympy.latex(sympy.simplify(sympy.sqrt(((x1 - x2) ** 2 + (y1 - y2) ** 2))))
+        return {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'razdalja': razdalja}
 
 
 class OblikeEnacbPremice(Naloga):  # TODO preveri jinja latex
@@ -105,33 +170,34 @@ class PremiceTrikotnik(Naloga):  # TODO preveri jinja latex
         else:
             k1 = sympy.Rational((y2 - y1), (x2 - x1))
             n1 = y1 - k1 * x1
-            premica1 = sympy.latex(sympy.Eq(y, k1 * x + n1))
+            premica1 = sympy.Eq(y, k1 * x + n1)
         if x3 == x2:
             premica2 = sympy.Eq(x, x3)
         else:
             k2 = sympy.Rational((y3 - y2), (x3 - x2))
             n2 = y3 - k2 * x3
-            premica2 = sympy.latex(sympy.Eq(y, k2 * x + n2))
+            premica2 = sympy.Eq(y, k2 * x + n2)
 
         ploscinaTrikotnika = sympy.simplify(abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2).evalf(3)
-        # TODO ne prikazuj nepotrebnih ničel v decimalnem zapisu
+        # TODO ne prikazuj nepotrebnih nul v decimalnem zapisu
 
-        return {'premica1': premica1, 'premica2': premica2, 'ploscina': ploscinaTrikotnika}
+        return {'premica1': sympy.latex(premica1), 'premica2': sympy.latex(premica2), 'ploscina': ploscinaTrikotnika}
 
 
 class NarisiLinearnoFukcijo(Naloga):
     def __init__(self, **kwargs):
         super().__init__(self, **kwargs)
-        self.besedilo_posamezne = jinja2.Template(r'''Nariši graf funkcije linearne ${{latex(naloga.linearnaF)}}$.''')
+        self.besedilo_posamezne = jinja2.Template(
+            r'''Nariši graf funkcije linearne $f(x) = {{latex(naloga.linearna)}}$.''')
         self.besedilo_vecih = jinja2.Template(r''' Nariši graf linearne funkcije:
         \begin{enumerate}
         {% for naloga in naloge %}
-        \item ${{latex(naloga.linearnaF)}}$
+        \item $f(x) = {{latex(naloga.linearna)}}$
         {% endfor %}
         \end{enumerate}
         ''')
 
-        self.resitev_posamezne = jinja2.Template(r'''${{latex(naloga.linearnaF)}}$\par
+        self.resitev_posamezne = jinja2.Template(r'''$f(x)={{latex(naloga.linearna)}}$\par
         \begin{minipage}{\linewidth}
         \centering
         \begin{tikzpicture}[baseline]
@@ -152,7 +218,7 @@ class NarisiLinearnoFukcijo(Naloga):
         self.resitev_vecih = jinja2.Template(r'''
         \begin{enumerate}
          {% for naloga in naloge %}
-         \item ${{latex(naloga.linearnaF)}}$\par
+         \item $f(x) = {{latex(naloga.linearna)}}$\par
          \begin{minipage}{\linewidth}
         \centering
         \begin{tikzpicture}[baseline]
@@ -178,14 +244,15 @@ class NarisiLinearnoFukcijo(Naloga):
         y = sympy.symbols('y')
         f = sympy.symbols('f(x)')
         nicla = (-n) / k
-        return {'linearnaF': sympy.Eq(f, eksplicitna), 'linearna': eksplicitna, 'n': n, 'nicla': nicla}
+        return {'linearna': eksplicitna, 'n': n, 'nicla': nicla}
 
 
 class VrednostiLinearne(Naloga):
     def __init__(self, **kwargs):
         super().__init__(self, **kwargs)
 
-        self.besedilo_posamezne = jinja2.Template(r'''Dana je funkcija s predpisom ${{naloga.linearna}}$. Izračunajte vrednost $f({{naloga.x1}})$ in za kateri $x$ je $f(x)={{naloga.y2}}$. Za katere vrednosti $x$ so vrednosti funkcije negativne? ''')
+        self.besedilo_posamezne = jinja2.Template(
+            r'''Dana je funkcija s predpisom ${{naloga.linearna}}$. Izračunajte vrednost $f({{naloga.x1}})$ in za kateri $x$ je $f(x)={{naloga.y2}}$. Za katere vrednosti $x$ so vrednosti funkcije negativne? ''')
 
         self.besedilo_vecih = jinja2.Template(r''' Za podano funkcijo $f$ izračunaj $f(x_0)$ in za kateri $x$ je $f(x)=y_1$. Za katere vrednosti $x$ so vrednosti funkcije negativne?
         \begin{enumerate}
@@ -218,9 +285,8 @@ class VrednostiLinearne(Naloga):
         preveri(x1 != x2)
         y1 = k * x1 + n
         y2 = k * x2 + n
-        negativno = sympy.latex(
-            sympy.solve_poly_inequality(sympy.Poly(funkcija, x), '<')[0])  # Solve vrne seznam intervalov
+
+        negativno = sympy.latex(sympy.solveset(funkcija < 0, domain=sympy.S.Reals))
         return {'linearna': sympy.latex(sympy.Eq(f, funkcija)), 'x1': sympy.latex(x1), 'x2': sympy.latex(x2),
                 'y1': sympy.latex(y1), 'y2': sympy.latex(y2), 'negativno': negativno}
 
-# print(VrednostiLinearne().poskusi_sestaviti())
