de106b6ea40eebcff6aba4630976402e309bb6b3
Ursa Pertot <ursa.pertot@gmail.com>
2019-05-06 01:38:11 +0200

    Autogeneriranje dokumentacije v Sphinx

---
+++ Sphinx/source/index.rst
@@ -0,0 +1,62 @@
+.. Dokumentacija za generiranje nalog documentation master file, created by
+   sphinx-quickstart on Sun May  5 23:04:55 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Dokumentacija za generiranje nalog's documentation!
+==============================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: generiranje
+    :members:
+..    :undoc-members:
+..    :inherited-members:
+..    :show-inheritance:
+
+.. .. automodule:: izpisi_naloge #TODO odkomentiraj ko izbrišeš zaganjanje funkcije v izpisi naloge
+    :members:
+
+.. automodule:: eksponentnaFunkcija
+    :members:
+
+.. automodule:: izrazi
+    :members:
+
+.. automodule:: kompleksnaStevila
+    :members:
+
+.. automodule:: kvadratnaFunkcija
+    :members:
+
+.. automodule:: linearnaFunkcija
+    :members:
+
+.. automodule:: mnozice
+    :members:
+
+.. automodule:: naravnaStevila
+    :members:
+
+.. automodule:: odvodi
+    :members:
+
+.. automodule:: polinom
+    :members:
+
+.. automodule:: stoznice
+    :members:
+
+.. automodule:: zaporedja
+    :members:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- generiranje.py
+++ generiranje.py
@@ -3,60 +3,103 @@ import sympy
 import importlib
 sympy_printing_latex = importlib.import_module('sympy.printing.latex')
 
-def mojLatex(izraz):
-    return sympy.latex(izraz).replace(r'\'','')
-    # def _print_tuple(self, expr):
-    #     return r"\left( %s\right)" % \
-    #         r", \  ".join([self._print(i) for i in expr])
+# def mojLatex(izraz):
+#     """
+#     Popravi izp
+#
+#     :type : int
+#     :param :
+#     """
+#     return sympy.latex(izraz).replace(r'\'','')
+#     # def _print_tuple(self, expr):
+#     #     return r"\left( %s\right)" % \
+#     #         r", \  ".join([self._print(i) for i in expr])
 
 class NapacnaNaloga(Exception):
+    """
+    Izjema, kadar sestavljena naloga ne ustreza pogojem.
+    """
     pass
 
 MinMaxNapaka = ValueError("Minimalna vrednost mora biti manjša ali enaka maksimalni vrednosti.")
 
 def preveri(pogoj):
+    """
+    Funkcija preveri, če sestavljena naloga ustreza pogojem, sicer javi napako NapacnaNaloga.
+
+    :type pogoj: Bool
+    :param pogoj: True, če naloga ustreza pogojem in False, kadar naloga ne ustreza pogojem
+    """
     if not pogoj:
         raise NapacnaNaloga
 
 
 class MojLatexPrinter(sympy_printing_latex.LatexPrinter):
+    """
+    Razred MojLatexPrinter je popravljena različica razreda LatexPrinter iz paketa sympy. Popravljeno izpisovanje tuple-ov brez presledka.
+    """
     def _print_tuple(self, expr):
         return r"\left( %s\right)" % r", ".join([self._print(i) for i in expr])
 
 def moj_latex(expr):
+    """
+    Funkcija kliče razred MojLatexPrinter in nastavi izpis naravnega logaritma kot *ln*
+
+    :type expr: str
+    :param expr: niz #TODO
+    """
     settings = {
-        # TODO: morda skopiraj še to?
         'ln_notation': True
     }
     return MojLatexPrinter(settings).doprint(expr)
 
 class Naloga:
-    """Razred Naloga ima atrubute za splošno besedilo posamezne, besedilo večih, rešitev posamezne in reštev večih."""
-    besedilo_posamezne = '''
-                        Reši nalogo: ${{ naloga }}$
-                    '''
-    besedilo_vecih = '''
-                        Reši sledeče naloge:
-                        \\begin{enumerate}
-                        {% for naloga in naloge %}
-                        \\item${{ naloga }}$
-                        {% endfor %}
-                        \\end{enumerate}
-                    '''
-    resitev_posamezne = '''
-                        Rešitev: ${{ naloga }}$ 
-                    '''
-    resitev_vecih = '''
-                        Rešitve nalog:
-                        \\begin{enumerate}
-                        {% for naloga in naloge %}
-                        \\item${{ naloga }}$
-                        {% endfor %}
-                        \\end{enumerate}
-                '''
+    """ Razred Naloga je splošni razred za posamezne naloge in vsebuje splošna besedila in rešitve nalog.
+
+    :param besedilo_posamezne: Splošno besedilo naloge
+    :type besedilo_posamezne: str
+    :param besedilo_vecih: Splošno besedilo za nalogo z več primeri
+    :type besedilo_vecih: str
+    :param resitev_posamezne: Splošno besedilo za rešitev naloge
+    :type resitev_posamezne: str
+    :param resitev_vecih: Splošno besedilo za rešitev naloge z več primeri
+    :type resitev_vecih: str
+
+    """
+    besedilo_posamezne = r'''Reši nalogo: ${{ naloga }}$'''
+    besedilo_vecih = r'''
+        Reši sledeče naloge:
+        \begin{enumerate}
+        {% for naloga in naloge %}
+        \item ${{ naloga }}$
+        {% endfor %}
+        \end{enumerate}
+        '''
+    resitev_posamezne = r'''Rešitev: ${{ naloga }}$'''
+    resitev_vecih = r'''Rešitve nalog:
+        \begin{enumerate}
+        {% for naloga in naloge %}
+        \item${{ naloga }}$
+        {% endfor %}
+        \end{enumerate}
+        '''
 
     def __init__(self, *args, besedilo_posamezne=None, besedilo_vecih=None, resitev_posamezne=None, resitev_vecih=None,
                  st_nalog=None, **kwargs):
+        """TODO
+
+        :param st_nalog: stevilo primerov posamezne naloge
+        :type st_nalog: int
+        :param besedilo_posamezne: besedilo naloge
+        :type besedilo_posamezne: str
+        :param besedilo_vecih: besedilo za nalogo z več primeri
+        :type besedilo_vecih: str
+        :param resitev_posamezne: besedilo za rešitev naloge
+        :type resitev_posamezne: str
+        :param resitev_vecih: besedilo za rešitev naloge z več primeri
+        :type resitev_vecih: str
+
+        """
         self.st_nalog = st_nalog
 
         if besedilo_posamezne is not None:
@@ -71,21 +114,18 @@ class Naloga:
         if resitev_vecih is not None:
             self.resitev_vecih = resitev_vecih
         
-        assert not args
-        assert not kwargs
-
-        # TODO: template se kliče samo enkrat - če kličeš na tem mestu besedilo_posamezne, je to objekt naloga in ne dejansko besedilo
-        # besedilo_posamezne =besedilo_posamezne)
-        # besedilo_vecih =besedilo_vecih)
-        # resitev_posamezne =resitev_posamezne)
-        # resitev_vecih =resitev_vecih)
+        if args:
+            raise ValueError('Pojavijo se neznani args.')
+        if kwargs:
+            raise ValueError('Pojavijo se neznani kwargs.')
 
 
     def poskusi_sestaviti(self):
+        """Poskusi sestaviti nalogo."""
         pass
 
     def sestavi(self):
-        """Sestavi nalogo ali v primeru Napacne naloge, poskuša sestaviti novo."""
+        """Sestavi nalogo, ki ustreza pogojem."""
         while True:
             try:
                 return self.poskusi_sestaviti()
@@ -93,24 +133,22 @@ class Naloga:
                 pass
 
     def sestavi_vec(self, stevilo_nalog):
-        """Metoda sestavi_vec sestavi več nalog.
+        """
+        Sestavi nalogo z več primeri
+
         :type stevilo_nalog: int
-        :param stevilo_nalog: koliko nalog določenega razreda želimo"""
+        :param stevilo_nalog: stevilo primerov posamezne naloge
+
+        """
         naloge = []
         for _ in range(stevilo_nalog):
             naloge.append(self.sestavi())
         return naloge
 
     def besedilo(self):
-        # TODO expand funkcija samo podalsuje senzame
-        # razsiritve = {
-        #     'latex': sympy.latex, 'expand': sympy.expand
-        # }
-        # print(self.besedilo_posamezne)
-        # self.besedilo_posamezne.globals.extend(razsiritve)
-        # self.besedilo_vecih.globals.extend(razsiritve)
-        # self.resitev_posamezne.globals.extend(razsiritve)
-        # self.resitev_vecih.globals.extend(razsiritve)
+        """
+        Ustvari predloge besedila in rešitev ter vanj vstavi vrednosti posameznih primerov.
+        """
         razsiritve = {
             'latex': moj_latex, 'expand': sympy.expand
         }
@@ -136,10 +174,3 @@ class Naloga:
 
             return {'naloga': template_besedilo_vecih.render(naloge=naloge),
                     'resitev': template_resitev_vecih.render(naloge=naloge)}
-
-# operatorji= {
-#     '+': lambda a, b: a + b,
-#     '-': lambda a, b: a - b,
-#     '\cdot': lambda a, b: a * b,
-#     '/': lambda a, b: a / b,
-# }
